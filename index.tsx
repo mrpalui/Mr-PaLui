7ce8b5f3d8b3f96f794645324722c6cbb09f8392
mrpalui <85637281+mrpalui@users.noreply.github.com>
2026-01-28 10:28:38 +0700

    feat: Initialize project with Vite and TypeScript
    Sets up the project with Vite as the build tool and TypeScript for type safety. Includes essential dependencies, configuration files, and basic HTML structure. Adds necessary CDN links for styling and functionality, and defines environment variables for API keys.

---
+++ index.tsx
@@ -0,0 +1,13 @@
+/*
+ * PENTING !!!
+ * Harap dicatat, aplikasi SULAP FOTO hanya untuk penggunaan pribadi!
+ *
+ * Pembelian resmi hanya melalui link berikut:
+ * https://ichsanlabs.com/sulapfoto
+ *
+ * PEMBERITAHUAN RESMI:
+ * Aplikasi SULAP FOTO dapat digunakan selama Gemini masih membuka aksesnya,
+ * dan SULAP FOTO dapat berhenti beroperasi sewaktu-waktu jika gemini menutup akses modelnya,
+ * Terima kasih.
+ * 
+ */
